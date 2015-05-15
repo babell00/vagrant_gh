99982bebbfe22e4ade70439099eda32a0dd23b6d
U-REALGAMES\jgajzler <jgajzler@pc010.realgames.int>
2015-05-15 11:25:15 +0200

    Add index file for tomcat

--- tomcat2/bootstrap.sh
+++ tomcat2/bootstrap.sh
@@ -9,8 +9,14 @@ fi
 echo "Installing git"
 apt-get install git -y
 
+echo "Cloning git reop"
+git clone https://github.com/babell00/vagrant_gh.git
+
 echo "Installing mc"
 apt-get install mc -y
 
 echo "Installing Tomcat 7"
 apt-get install  tomcat7 -y
+
+echo "Replacing index file in tomcat"
+mv /home/vagrant/vagrant_gh/tomcat1/tomcat/index.html /var/lib/tomcat7/webapps/ROOT/index.html
